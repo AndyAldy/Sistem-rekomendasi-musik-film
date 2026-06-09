075e70d08ac9bdafafcc4782f54ca2d8bb5aa9fe
AndyAldy <andyaldyansyah11@gmail.com>
2026-06-09 19:08:30 +0700

    edited

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
+// Ubah bagian require menjadi import
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
 
 const app = express();
 app.use(cors()); 
